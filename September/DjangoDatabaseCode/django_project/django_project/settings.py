64fe16226cd7f5c1757770c0040ca6db88c47860
LuisRom <62123213+Lampara@users.noreply.github.com>
2022-10-07 16:08:35 -0500

    Added password reset
    Added password reset and used environmental variables

--- September/DjangoDatabaseCode/django_project/django_project/settings.py
+++ September/DjangoDatabaseCode/django_project/django_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o9_snryf+%i0rh#jd6rw!!s5imcf++qigu@eskz8b*k6nkvls7'
+SECRET_KEY = os.environ['TestEmailPassword']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -131,3 +131,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ['TestEmail']
+EMAIL_HOST_PASSWORD = os.environ['TestEmailPassword']
